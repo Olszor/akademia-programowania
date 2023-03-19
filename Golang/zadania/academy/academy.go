1659ab54e163eb022fbde4bdfadc6016465cac26
Adam Olszewski <adas2408@gmail.com>
2023-03-19 16:52:24 +0100

    Task 1 completed

--- Golang/zadania/academy/academy.go
+++ Golang/zadania/academy/academy.go
@@ -1,5 +1,9 @@
 package academy
 
+import (
+	"math"
+)
+
 type Student struct {
 	Name       string
 	Grades     []int
@@ -11,7 +15,14 @@ type Student struct {
 // slice containing all grades received during a
 // semester, rounded to the nearest integer.
 func AverageGrade(grades []int) int {
-	panic("not implemented")
+	if len(grades) == 0 {
+		return 0
+	}
+	sum := 0.0
+	for _, value := range grades {
+		sum += float64(value)
+	}
+	return int(math.Round(sum / float64(len(grades))))
 }
 
 // AttendancePercentage returns a percentage of class
@@ -21,7 +32,13 @@ func AverageGrade(grades []int) int {
 // The percentage of attendance is represented as a
 // floating-point number ranging from 0 to 1.
 func AttendancePercentage(attendance []bool) float64 {
-	panic("not implemented")
+	present := 0.0
+	for _, value := range attendance {
+		if value {
+			present += 1
+		}
+	}
+	return present / float64(len(attendance))
 }
 
 // FinalGrade returns a final grade achieved by a student,
@@ -36,12 +53,23 @@ func AttendancePercentage(attendance []bool) float64 {
 // decreased by 1. If the student's attendance is below 60%, average
 // grade is 1 or project grade is 1, the final grade is 1.
 func FinalGrade(s Student) int {
-	panic("not implemented")
+	if AttendancePercentage(s.Attendance) < 0.6 || s.Project == 1 || AverageGrade(s.Grades) == 1 {
+		return 1
+	}
+	finalGrade := int(math.Round(float64(s.Project + AverageGrade(s.Grades)) / 2.0))
+	if AttendancePercentage(s.Attendance) < 0.8 {
+		finalGrade--
+	}
+	return finalGrade
 }
 
 // GradeStudents returns a map of final grades for a given slice of
 // Student structs. The key is a student's name and the value is a
 // final grade.
 func GradeStudents(students []Student) map[string]uint8 {
-	panic("not implemented")
+	gradesMap := make(map[string]uint8)
+	for _, student := range students {
+		gradesMap[student.Name] = uint8(FinalGrade(student))
+	}
+	return gradesMap
 }
