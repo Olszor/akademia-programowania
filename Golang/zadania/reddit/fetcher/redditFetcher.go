69e2aa32f8b2e1c5714883837440e1cf9bbf0edc
Olszor <adas2408@gmail.com>
2023-04-02 17:59:08 +0200

    Task 2 completed

--- Golang/zadania/reddit/fetcher/redditFetcher.go
+++ Golang/zadania/reddit/fetcher/redditFetcher.go
@@ -1,7 +1,12 @@
 package fetcher
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"io"
+	"log"
+	"net/http"
 )
 
 type response struct {
@@ -18,4 +23,74 @@ type response struct {
 type RedditFetcher interface {
 	Fetch() error
 	Save(io.Writer) error
+	FetchWithContext(ctx context.Context) error
+}
+
+type Fetcher struct {
+	Client   http.Client
+	Url      string
+	response response
+}
+
+func (f *Fetcher) Fetch() error {
+	req, err := http.NewRequest(http.MethodGet, f.Url, http.NoBody)
+	if err != nil {
+		return fmt.Errorf("error creating request: %s", err)
+	}
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := f.Client.Do(req)
+	if err != nil {
+		return fmt.Errorf("error fetching data: %s", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	var data response
+	err = json.NewDecoder(resp.Body).Decode(&data)
+	if err != nil {
+		return fmt.Errorf("error reading data: %s", err)
+	}
+	f.response = data
+
+	return nil
+}
+
+func (f *Fetcher) Save(writer io.Writer) error {
+	for _, child := range f.response.Data.Children {
+		_, err := writer.Write([]byte(fmt.Sprintf("%s\n%s\n", child.Data.Title, child.Data.URL)))
+		if err != nil {
+			return fmt.Errorf("error writing to a file %s", err)
+		}
+	}
+	log.Printf("Saved succesfully!")
+	return nil
+}
+
+func (f *Fetcher) FetchWithContext(ctx context.Context) error {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, f.Url, http.NoBody)
+	if err != nil {
+		return fmt.Errorf("error creating request: %s", err)
+	}
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := f.Client.Do(req)
+	if err != nil {
+		return fmt.Errorf("error fetching data: %s", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	var data response
+	err = json.NewDecoder(resp.Body).Decode(&data)
+	if err != nil {
+		return fmt.Errorf("error reading data: %s", err)
+	}
+	f.response = data
+
+	return nil
 }
--- Golang/zadania/reddit/main.go
+++ Golang/zadania/reddit/main.go
@@ -1,14 +1,56 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"io"
+	"log"
+	"net/http"
+	"os"
 	"reddit/fetcher"
+	"sync"
+	"time"
 )
 
 func main() {
-	var f fetcher.RedditFetcher // do not change
-	var w io.Writer             // do not change
+	//var f fetcher.RedditFetcher // do not change
+	var w io.Writer // do not change
 
-	f.Fetch()
-	f.Save(w)
+	subreddits := []string{
+		"golang",
+		"java",
+		"csharp",
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(len(subreddits))
+
+	for _, subreddit := range subreddits {
+		go func(sub string) {
+			defer wg.Done()
+			f := &fetcher.Fetcher{
+				Client: http.Client{},
+				Url:    fmt.Sprintf("https://www.reddit.com/r/%s.json", sub),
+			}
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+			defer cancel()
+
+			err := f.FetchWithContext(ctx)
+			if err != nil {
+				log.Printf(err.Error())
+			}
+
+			w, err = os.Create(fmt.Sprintf("./data/%s.txt", sub))
+			if err != nil {
+				log.Printf(err.Error())
+			}
+
+			err = f.Save(w)
+			if err != nil {
+				log.Printf(err.Error())
+			}
+		}(subreddit)
+	}
+	wg.Wait()
+
 }
